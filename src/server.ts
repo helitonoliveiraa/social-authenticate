59f6255b5ecaff69ac1d2612c97d1639bff781cd
Héliton Oliveira <heliton.oliveira88@gmail.com>
2023-02-01 17:48:34 -0300

    refactor: remove obsolete code

--- src/server.ts
+++ src/server.ts
@@ -12,28 +12,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// app.use(session({
-//   secret: 'key that will sign cookie',
-//   resave: false,
-//   saveUninitialized: false,
-//   cookie: { maxAge: 60 * 60 * 1 }
-// }));
-
-// app.use(cookieSession({
-//   secret: process.env.JWT_SECRET,
-//   maxAge: 60 * 60 * 24,
-//   secure: false,
-// }));
-
 app.use(router);
 app.use(fakeBrowser);
-
-// app.get('/test', (req, res) => {
-//   req.session.isAuth = true;
-//   console.log(req.session);
-//   console.log(req.session.id);
-//   res.send('Heelooo sessions');
-// });
 
 app.listen(4000, () => {
   console.log('Server running...');
